ddd82821418674dbf5bdd8bd534ebce1540ec6bb
nebilsefawdin-ops <nebilsefawdin@gmail.com>
2026-06-13 00:29:27 +0300

    Create transpose matrix.py

---
+++ transpose matrix.py
@@ -0,0 +1,11 @@
+class Solution:
+    def transpose(self, matrix: List[List[int]]) -> List[List[int]]:
+        m=len(matrix)
+        n=(len(matrix[0]))
+
+        transpose = [[0]*m for k in range(n)]
+
+        for i in range (m):
+            for j in range (n):
+                transpose[j][i]=matrix[i][j]
+        return transpose
