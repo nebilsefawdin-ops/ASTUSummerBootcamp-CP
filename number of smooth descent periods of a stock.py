711f810e2fd2f37215be6580508df4591dd7d37a
Nebil Sefawudin <nebilsefawdin@gmail.com>
2026-06-28 01:02:50 +0300

    Create number of smooth descent periods of a stock.py

---
+++ number of smooth descent periods of a stock.py
@@ -0,0 +1,14 @@
+class Solution:
+    def getDescentPeriods(self, prices: List[int]) -> int:
+        count = 1
+        ans = 1
+
+        for i in range(1, len(prices)):
+            if prices[i - 1] - prices[i] == 1:
+                count += 1
+            else:
+                count = 1
+
+            ans += count
+
+        return ans
